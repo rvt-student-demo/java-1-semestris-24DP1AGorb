c4ccd6fb1af971a113192cf76aad5424b7653bd1
Artūrs Gorbačovs <A240166@rvt.lv>
2025-09-16 14:38:34 +0300

    --

---
+++ src/main/java/rvt/additionss.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class additionss {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ievadi pirmo skaitli:");
+        int pirmais = Integer.valueOf(scanner.nextLine());
+        System.out.println("Ievadi otro skaitli:");
+        int otrais = Integer.valueOf(scanner.nextLine());
+        int result = pirmais + otrais;
+        System.out.println("Rezultats ir " + result);
+        scanner.close();
+
+
+    }
+
+}
+
